53a7c079d72856a2ff41186c0b5de033a0ad9ba5
Sonic4051 <keng-yok@hotmail.com>
2020-07-24 16:00:40 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "amic3db.chksgzqjwzak.us-east-2.rds.amazonaws.com";
+$servername = "amic3db.chksgzqjwzak.us-east-2.rds.amazonaws.com:3306";
 $username = "sonic4051";
 $password = "keng4051";
 $dbname = "amic3";
