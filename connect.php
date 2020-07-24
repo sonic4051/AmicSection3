8d4cf544f9d9229098939b569ffaa3757901dd45
Sonic4051 <keng-yok@hotmail.com>
2020-07-24 16:02:49 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,5 @@
 <?php
-$servername = "amic3db.chksgzqjwzak.us-east-2.rds.amazonaws.com:3306";
-$username = "sonic4051";
-$password = "keng4051";
-$dbname = "amic3";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-
-}else echo "Success";
+else echo"Success";
 $webURL="https://amic3server.ngrok.io/";
 
 ?>
