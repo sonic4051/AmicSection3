c2a320f150e4f1ebf4a89cfb330009838b063b06
Sonic4051 <keng-yok@hotmail.com>
2020-07-29 19:06:38 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -6,6 +6,7 @@ $dbname = "amic_bot_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-if($conn) echo"DONE";
+if(!$conn) echo"NOT DONE";
+else echo"DONE ALL";
 
 ?>
