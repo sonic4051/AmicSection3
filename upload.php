9c244781652826efa8f550ed6b9df13735e04b02
Sonic4051 <keng-yok@hotmail.com>
2020-07-29 23:03:57 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -20,7 +20,7 @@
 			'SourceFile' => $temp_file_location			
 		]);
 		//var_dump($result);
-		if($result!=NULL && $result!="") echo"อับโหลดสำเร็จ1";
+		if($result!=NULL and $result!="") echo"อับโหลดสำเร็จ1";
 	}
 ?>
 
