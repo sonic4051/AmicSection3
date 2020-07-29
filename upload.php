4ee67a0bc58619c8957fe6a45425c917cb2be283
Sonic4051 <keng-yok@hotmail.com>
2020-07-29 21:03:30 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,15 @@
 <?php
 
 require 'vendor/autoload.php';
+use Aws\S3\S3Client;
  
+$bucketName = 'YOUR_BUCKET_NAME';
+$client = new S3Client([
+    'version' => 'latest',
+    'region' => 'YOUR_AWS_REGION',
+    'credentials' => [
+        'key'    => 'ACCESS_KEY_ID',
+        'secret' => 'SECRET_ACCESS_KEY'
+    ]
+]); 
 ?>
