44450693cd928af2e6798b44d480061c5a6a0a45
Sonic4051 <keng-yok@hotmail.com>
2020-07-29 20:54:59 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -14,22 +14,4 @@ $s3 = new S3Client([
     ]
 ]);
  
- 
-$bucketName = 'amic-bot-storage';
-$file_Path = __DIR__ . '/item.PNG';
-$key = basename($file_Path);
- 
-// Upload a publicly accessible file. The file size and type are determined by the SDK.
-try {
-    $result = $s3->putObject([
-        'Bucket' => $bucketName,
-        'Key'    => $key,
-        'Body'   => fopen($file_Path, 'r'),
-        'ACL'    => 'public-read',
-    ]);
-    echo $result->get('ObjectURL');
-} catch (Aws\S3\Exception\S3Exception $e) {
-    echo "There was an error uploading the file.\n";
-    echo $e->getMessage();
-}
 ?>
