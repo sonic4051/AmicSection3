f5fad0365edb6a67a812cb062e47e69f94083f11
Sonic4051 <keng-yok@hotmail.com>
2020-07-30 00:53:42 +0700

    Create viewfile.php

---
+++ viewfile.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$bucket = 'amic-bot-storage';
+$keyname = 'AKIA4YXAPMXBTXNUKZEB';
+
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'ap-southeast-1'
+]);
+
+try {
+    // Get the object.
+    $result = $s3->getObject([
+        'Bucket' => $bucket,
+        'Key'    => $keyname
+    ]);
+
+    // Display the object in the browser.
+    header("Content-Type: {$result['ContentType']}");
+    echo $result['Body'];
+} catch (S3Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
+
+?>
