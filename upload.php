5d6efb511ab2b6fc963809e074693f2e10958b75
Sonic4051 <keng-yok@hotmail.com>
2020-07-29 21:10:09 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -12,4 +12,21 @@ $client = new S3Client([
         'secret' => 'ifzbbZRzCR0r9MU0pzdsgEfyRuK+V2ZtxW5FvdLG'
     ]
 ]); 
+
+$file_Path = 'item.png';
+$key = basename($file_Path);
+ 
+// Upload a publicly accessible file. The file size and type are determined by the SDK.
+try {
+    $result = $s3->putObject([
+        'Bucket' => $bucketName,
+        'Key'    => $key,
+        'Body'   => fopen($file_Path, 'r'),
+        'ACL'    => 'public-read',
+    ]);
+    echo $result->get('ObjectURL');
+} catch (Aws\S3\Exception\S3Exception $e) {
+    echo "There was an error uploading the file.\n";
+    echo $e->getMessage();
+}
 ?>
