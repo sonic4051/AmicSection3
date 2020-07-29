85061e2f113fd244c00f7a98f2bf5b3cf6db218b
Sonic4051 <keng-yok@hotmail.com>
2020-07-30 02:13:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 namespace LINE\LINEBot;
 header('Content-Type: text/html; charset=utf-8');
 require_once 'vendor/autoload.php';
+$accessToken = "i01ExIyKX9/iOZ/z+sZVY/yxfx3QIGuxSAKzNM29JmBlk2ZK1aO9gLQt9uf3kJl5MpHwv0BqWkV4/55N4BSjxs9NaRLM+6yWLplwWZTTwylAJxy9djgppCsbYQSJeRvs7hWU5hCov1JxZkx1ZXIVVwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+$channelSecret = "1e3afa9e6459fba7b178fa4e940762c4";
+//file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$content = file_get_contents('php://input');
 // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
 $events = json_decode($content, true);
 if(!is_null($events)){
