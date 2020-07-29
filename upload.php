96e562756d9fc69db00a253003bde391a2a1866f
Sonic4051 <keng-yok@hotmail.com>
2020-07-29 20:57:14 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,16 +2,4 @@
 
 require 'vendor/autoload.php';
  
-use Aws\S3\S3Client;
- 
-// Instantiate an Amazon S3 client.
-$s3 = new S3Client([
-    'version' => 'latest',
-    'region'  => 'ap-southeast-1',
-    'credentials' => [
-        'key'    => 'AKIA4YXAPMXBTXNUKZEB',
-        'secret' => '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'
-    ]
-]);
- 
 ?>
