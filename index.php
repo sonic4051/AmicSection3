4ccccb8a412d28d46ec156115cab688cac50ef2d
Sonic4051 <keng-yok@hotmail.com>
2020-07-30 02:05:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,85 +1,100 @@
 <?php 
-	/*Get Data From POST Http Request*/
-	$datas = file_get_contents('php://input');
-	/*Decode Json From LINE Data Body*/
-	$deCode = json_decode($datas,true);
-
-	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
-
-	$replyToken = $deCode['events'][0]['replyToken'];
-	$userId = $deCode['events'][0]['source']['userId'];
-	$text = $deCode['events'][0]['message']['text'];
-//real url change
-	$url=$text;
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HEADER, true);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$a = curl_exec($ch); // $a will contain all headers
-	$url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-//end of change	
-	$messages = [];
-	$messages['replyToken'] = $replyToken;
-	$messages['messages'][0] = getFormatTextMessage($url);
-
-	$encodeJson = json_encode($messages);
-
-	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
-  	$LINEDatas['token'] = "hiCo5SaKOlgH0Lxjjl3VvpcxFijj2B00ouHm24f62sQ+SrdjTJOgS5AHX8v88fuZJuXHExYi99mAAddQ3qalql3Sw49OdaVxxveCw3voJtTA+3oxUEp22jIUS2qpR6jQTS3N2HZVVUfne5F+ZtwJwAdB04t89/1O/w1cDnyilFU=";
-
-  	$results = sentMessage($encodeJson,$LINEDatas);
-
-	/*Return HTTP Request 200*/
-	http_response_code(200);
-
-	function getFormatTextMessage($text)
-	{
-		$datas = [];
-		$datas['type'] = 'text';
-		$datas['text'] = $text;
-
-		return $datas;
-	}
-
-	function sentMessage($encodeJson,$datas)
-	{
-		$datasReturn = [];
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => $datas['url'],
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "POST",
-		  CURLOPT_POSTFIELDS => $encodeJson,
-		  CURLOPT_HTTPHEADER => array(
-		    "authorization: Bearer ".$datas['token'],
-		    "cache-control: no-cache",
-		    "content-type: application/json; charset=UTF-8",
-		  ),
-		));
-
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
-
-		curl_close($curl);
-
-		if ($err) {
-		    $datasReturn['result'] = 'E';
-		    $datasReturn['message'] = $err;
-		} else {
-		    if($response == "{}"){
-			$datasReturn['result'] = 'S';
-			$datasReturn['message'] = 'Success';
-		    }else{
-			$datasReturn['result'] = 'E';
-			$datasReturn['message'] = $response;
-		    }
-		}
-
-		return $datasReturn;
-	}
+// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+$events = json_decode($content, true);
+if(!is_null($events)){
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
+    $typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];
+    $userMessage = strtolower($userMessage);
+    switch ($typeMessage){
+        case 'text':
+            switch ($userMessage) {
+                case "t":
+				
+					$url="https://ift.tt/393BoPZ";
+					$ch = curl_init();
+					curl_setopt($ch, CURLOPT_URL, $url);
+					curl_setopt($ch, CURLOPT_HEADER, true);
+					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					$a = curl_exec($ch); // $a will contain all headers
+					$url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // This is what you need, it will return you the last effective URL
+                    $textReplyMessage = $url;
+                    $replyData = new TextMessageBuilder($textReplyMessage);
+                    break;
+                case "i":
+                    $picFullSize = 'https://amic-bot-storage.s3-ap-southeast-1.amazonaws.com/test.PNG';
+                    $picThumbnail = 'https://amic-bot-storage.s3-ap-southeast-1.amazonaws.com/test.PNG';
+                    $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
+                    break;
+                case "v":
+                    $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/240';
+                    $videoUrl = "https://www.mywebsite.com/simplevideo.mp4";                
+                    $replyData = new VideoMessageBuilder($videoUrl,$picThumbnail);
+                    break;
+                case "a":
+                    $audioUrl = "https://www.mywebsite.com/simpleaudio.mp3";
+                    $replyData = new AudioMessageBuilder($audioUrl,27000);
+                    break;
+                case "l":
+                    $placeName = "ที่ตั้งร้าน";
+                    $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
+                    $latitude = 13.780401863217657;
+                    $longitude = 100.61141967773438;
+                    $replyData = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);              
+                    break;
+                case "s":
+                    $stickerID = 22;
+                    $packageID = 2;
+                    $replyData = new StickerMessageBuilder($packageID,$stickerID);
+                    break;      
+                case "im":
+                    $imageMapUrl = 'https://www.mywebsite.com/imgsrc/photos/w/sampleimagemap';
+                    $replyData = new ImagemapMessageBuilder(
+                        $imageMapUrl,
+                        'This is Title',
+                        new BaseSizeBuilder(699,1040),
+                        array(
+                            new ImagemapMessageActionBuilder(
+                                'test image map',
+                                new AreaBuilder(0,0,520,699)
+                                ),
+                            new ImagemapUriActionBuilder(
+                                'http://www.ninenik.com',
+                                new AreaBuilder(520,0,520,699)
+                                )
+                        )); 
+                    break;          
+                case "tm":
+                    $replyData = new TemplateMessageBuilder('Confirm Template',
+                        new ConfirmTemplateBuilder(
+                                'Confirm template builder',
+                                array(
+                                    new MessageTemplateActionBuilder(
+                                        'Yes',
+                                        'Text Yes'
+                                    ),
+                                    new MessageTemplateActionBuilder(
+                                        'No',
+                                        'Text NO'
+                                    )
+                                )
+                        )
+                    );
+                    break;                                                                                                                          
+                default:
+                    $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
+                    $replyData = new TextMessageBuilder($textReplyMessage);         
+                    break;                                      
+            }
+            break;
+        default:
+            $textReplyMessage = json_encode($events);
+            $replyData = new TextMessageBuilder($textReplyMessage);         
+            break;  
+    }
+}
+//l ส่วนของคำสั่งตอบกลับข้อความ
+$response = $bot->replyMessage($replyToken,$replyData);
 ?>
