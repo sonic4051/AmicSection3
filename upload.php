dfeeb2749b9629293fbdbd545e13a222dac9d392
Sonic4051 <keng-yok@hotmail.com>
2020-07-29 20:50:29 +0700

    test uplaod

--- upload.php
+++ upload.php
@@ -1,50 +1,35 @@
 <?php
-// Require the Composer autoloader.
-require 'vendor/autoload.php';
-use Aws\S3\S3Client;
-use Aws\Exception\AwsException;
-use Aws\S3\Exception\S3Exception;
 
+require 'vendor/autoload.php';
+ 
+use Aws\S3\S3Client;
+ 
+// Instantiate an Amazon S3 client.
 $s3 = new S3Client([
-'version' => 'latest',
-'region' => 'ap-southeast-1',
-'credentials' => [
-'key' => 'AKIA4YXAPMXBTXNUKZEB',
-'secret' => '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'
-]
+    'version' => 'latest',
+    'region'  => 'ap-southeast-1',
+    'credentials' => [
+        'key'    => 'AKIA4YXAPMXBTXNUKZEB',
+        'secret' => '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'
+    ]
 ]);
-// Upload an object to Amazon S3
-$bucket = 'amic-bot-storage';
+ 
+ 
+$bucketName = 'amic-bot-storage';
+$file_Path = __DIR__ . '/item.PNG';
+$key = basename($file_Path);
+ 
+// Upload a publicly accessible file. The file size and type are determined by the SDK.
 try {
-//$s3->createBucket(['Bucket' => 'gangimg']);
-//echo 'Create bucket';
-// Upload an object to Amazon S3
-$filename = explode(".", $_FILES['img']["name"]);
-$filenameext = $filename[count($filename) — 1];
-$filename = $_POST['path'].'img_' . time() . "." . $filenameext;
-$result = $s3->putObject(array(
-'Bucket' => $bucket,
-'Key' => $filename,
-'ACL' => 'public-read',
-'SourceFile' => $_FILES['img']['tmp_name'],
-));
-// Access parts of the result object
-echo $result['Expiration'] . "\n";
-echo $result['ServerSideEncryption'] . "\n";
-echo $result['ETag'] . "\n";
-echo $result['VersionId'] . "\n";
-echo $result['RequestId'] . "\n";
-// Get the URL the object can be downloaded from
-echo $result['ObjectURL'] . "\n";
-echo '<img src="' . $result['ObjectURL'] . '">';
-} catch (S3Exception $e) {
-// Catch an S3 specific exception.
-echo $e->getMessage();
-} catch (AwsException $e) {
-// This catches the more generic AwsException. You can grab information
-// from the exception using methods of the exception object.
-echo $e->getAwsRequestId() . "\n";
-echo $e->getAwsErrorType() . "\n";
-echo $e->getAwsErrorCode() . "\n";
+    $result = $s3->putObject([
+        'Bucket' => $bucketName,
+        'Key'    => $key,
+        'Body'   => fopen($file_Path, 'r'),
+        'ACL'    => 'public-read',
+    ]);
+    echo $result->get('ObjectURL');
+} catch (Aws\S3\Exception\S3Exception $e) {
+    echo "There was an error uploading the file.\n";
+    echo $e->getMessage();
 }
 ?>
