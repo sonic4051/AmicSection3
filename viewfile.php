5cc4e730d9e04b11b8805890c46c516d8f6734d7
Sonic4051 <keng-yok@hotmail.com>
2020-07-30 01:37:09 +0700

    Update viewfile.php

--- viewfile.php
+++ viewfile.php
@@ -2,17 +2,32 @@
 
 require 'vendor/autoload.php';
 
-use Aws\S3\S3Client;
-use Aws\S3\Exception\S3Exception;
+use Aws\S3\S3Client;  
+use Aws\Exception\AwsException;
 
-$bucket = 'amic-bot-storage';
-$key = 'AKIA4YXAPMXBTXNUKZEB';
-$secret = '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';
+define('AWS_KEY', 'AKIA4YXAPMXBTXNUKZEB');
+define('AWS_SECRET_KEY', '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');
+define('HOST', 'https://amic-bot-storage.s3-ap-southeast-1.amazonaws.com');
+define('REGION', 'ap-southeast-1');
 
-$s3 = new AmazonS3($key, $secret);
-$objInfo = $s3->get_object_headers($bucket, 'test.PNG');
-$obj = $s3->get_object($bucket, 'test.PNG');
-header('Content-type: ' . $objInfo->header['_info']['content_type']);
-echo $obj->body;
+// Establish connection with DreamObjects with an S3 client.
+$client = new Aws\S3\S3Client([
+    'version'     => '2006-03-01',
+    'region'      => REGION,
+    'endpoint'    => HOST,
+        'credentials' => [
+        'key'      => AWS_KEY,
+        'secret'   => AWS_SECRET_KEY,
+    ]
+]);
 
+$plain_url = $client->getObjectUrl('amic-bot-storage', 'test.PNG');
+echo $plain_url . "\n";
+
+$cmd = $client->getCommand('GetObject', [
+        'Bucket' => 'amic-bot-storage',
+        'Key'    => 'test.PNG'
+]);
+$signed_url = $client->createPresignedRequest($cmd, '+1 hour');
+echo $signed_url->getUri() . "\n";
 ?>
