698b023f089a3a7bdf0abc69f926fb5c19725823
Sonic4051 <keng-yok@hotmail.com>
2020-07-30 04:16:08 +0700

    Update viewfile.php

--- viewfile.php
+++ viewfile.php
@@ -25,9 +25,10 @@ $keyname = 'n.jpg';
     // Display the object in the browser.
     header("Content-Type: {$result['ContentType']}");
     echo $result['Body'];
-	} catch (S3Exception $e) {
-		echo $e->getMessage() . PHP_EOL;
-	}
+	} 
+	//catch (S3Exception $e) {
+	//	echo $e->getMessage() . PHP_EOL;
+	//}
 
 
 ?>
