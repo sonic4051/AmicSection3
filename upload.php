ab2c4e0c55f15d8c1b801d20bf5afae4189bb3eb
Sonic4051 <keng-yok@hotmail.com>
2020-07-29 21:48:07 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,32 +1,27 @@
 <?php
 
+// Include the SDK using the composer autoloader
 require 'vendor/autoload.php';
-use Aws\S3\S3Client;
- 
-$bucketName = 'amic-bot-storage';
-$client = new S3Client([
-    'version' => 'latest',
-    'region' => 'ap-southeast-1',
-    'credentials' => [
-        'key'    => 'AKIA4YXAPMXBTXNUKZEB',
-        'secret' => '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'
-    ]
-]); 
 
-$file_Path = 'Item.PNG';
-$key = basename($file_Path);
- 
-// Upload a publicly accessible file. The file size and type are determined by the SDK.
-try {
-    $result = $s3->putObject([
-        'Bucket' => $bucketName,
-        'Key'    => $key,
-        'Body'   => fopen($file_Path, 'r'),
-        'ACL'    => 'public-read',
-    ]);
-    echo $result->get('ObjectURL');
-} catch (Aws\S3\Exception\S3Exception $e) {
-    echo "There was an error uploading the file.\n";
-    echo $e->getMessage();
-}
+$s3 = new Aws\S3\S3Client([
+	'region'  => 'ap-southeast-1',
+	'version' => 'latest',
+	'credentials' => [
+	    'key'    => "AKIA4YXAPMXBTXNUKZEB",
+	    'secret' => "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",
+	]
+]);
+
+// Send a PutObject request and get the result object.
+$key = 'sonicboom';
+
+$result = $s3->putObject([
+	'Bucket' => 'amic-bot-storage',
+	'Key'    => $key,
+	'Body'   => 'this is the body!',
+	//'SourceFile' => 'c:\samplefile.png' -- use this if you want to upload a file from a local location
+]);
+
+// Print the body of the result by indexing into the result object.
+var_dump($result);
 ?>
