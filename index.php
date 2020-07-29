c3146d158b7e9068bc1ee9dacf5821f60264a7a6
Sonic4051 <keng-yok@hotmail.com>
2020-07-30 02:35:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $accessToken = "i01ExIyKX9/iOZ/z+sZVY/yxfx3QIGuxSAKzNM29JmBlk2ZK1aO9gLQt9uf3kJl5MpHwv0BqWkV4/55N4BSjxs9NaRLM+6yWLplwWZTTwylAJxy9djgppCsbYQSJeRvs7hWU5hCov1JxZkx1ZXIVVwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    $accessToken = "hiCo5SaKOlgH0Lxjjl3VvpcxFijj2B00ouHm24f62sQ+SrdjTJOgS5AHX8v88fuZJuXHExYi99mAAddQ3qalql3Sw49OdaVxxveCw3voJtTA+3oxUEp22jIUS2qpR6jQTS3N2HZVVUfne5F+ZtwJwAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
     
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
