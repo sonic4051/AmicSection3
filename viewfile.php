04f233ea6ae677201f33b0ae27d15166eb389880
Sonic4051 <keng-yok@hotmail.com>
2020-07-30 13:21:20 +0700

    Update viewfile.php

--- viewfile.php
+++ viewfile.php
@@ -2,4 +2,4 @@
 $bucket = 'amic-bot-storage';
 $keyname = '1.PNG';
 ?>
-<img src="<?php echo"https://".$bucket.".s3-ap-southeast-1.amazonaws.com/".$keyname; ?>" alt="Girl in a jacket" width="500" height="600">
+<img src="<?php echo"https://".$bucket.".s3-ap-southeast-1.amazonaws.com/".$keyname; ?>">
