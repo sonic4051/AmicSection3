a6512598d5ec0eef3fa74ce225689ed4c4713465
Sonic4051 <keng-yok@hotmail.com>
2020-07-30 13:17:15 +0700

    backup viewfile

---
+++ viewfile_backup.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'vendor/autoload.php';
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$bucket = 'amic-bot-storage';
+$keyname = 'n.jpg';
+
+	$s3 = new Aws\S3\S3Client([
+		'region'  => 'ap-southeast-1',
+		'version' => 'latest',
+		'credentials' => [
+			'key'    => "AKIA4YXAPMXBTXNUKZEB",
+			'secret' => "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",
+			]
+		]);	
+		
+	try {
+    // Get the object.
+    $result = $s3->getObject([
+        'Bucket' => $bucket,
+        'Key'    => $keyname
+    ]);
+    // Display the object in the browser.
+    header("Content-Type: {$result['ContentType']}");
+    echo $result['Body'];
+	} 
+	catch (S3Exception $e) {
+		echo $e->getMessage() . PHP_EOL;
+	}
+	var_dump($result);
+?>
