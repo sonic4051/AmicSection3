913799739e530519c5430241219aa60f3ff21565
Sonic4051 <keng-yok@hotmail.com>
2020-08-02 16:24:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -179,8 +179,8 @@ if($Passport=="true") {
                                           //$cutstr = mb_strimwidth($ReplyData1, 0,1500, "...", "UTF-8"); //ตัดคำให้เหลือ 1500
                                           //$EditContent = $cutstr."\n (".$LinkNews.")";
                                           //$ReplyData = $EditContent;
-										  $cutstr = mb_substr($string, 0, 1800,'utf-8');
-										  $ReplyData = $cutstr;
+										  $cutstr = mb_substr($string, 0, 1500,'utf-8');
+										  $ReplyData = $countStr.$cutstr;
                                         } else {
                                             $ReplyData = $ReplyData1;
 											//$ReplyData = $countStr;
