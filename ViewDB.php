8a1d21390f2d04a6089d000b44f8a1c55125bfc6
Sonic4051 <keng-yok@hotmail.com>
2020-08-02 16:50:15 +0700

    Update ViewDB.php

--- ViewDB.php
+++ ViewDB.php
@@ -6,7 +6,6 @@ if(isset($_POST['submit']))
 	$NewsID = $_POST['NewsID'];
 	$sql = "SELECT * FROM news WHERE News_id = '$NewsID'";
     $result = $conn->query($sql);
-	//mb_internal_encoding('utf-8');
     if ($result->num_rows > 0) 
 	{
         while($row = $result->fetch_assoc()) 
@@ -17,11 +16,12 @@ if(isset($_POST['submit']))
             $IDReporter = $row["NewsReporter"];
 		}
 	}
-	//$countStr = mb_strlen($NewContent, 'utf-8');
-	//$NewContent1 = mb_substr($NewContent, 0, 1500,'utf-8');
-	//echo $countStr."<br>";	
-	//echo"หัวข้อข่าว :: ".$NewHeader."<br>";
-	//echo $NewContent;
+	mb_internal_encoding('utf-8');
+	$countStr = mb_strlen($NewContent, 'utf-8');
+	$NewContent1 = mb_substr($NewContent, 0, 1500,'utf-8');
+	echo $countStr."<br>";	
+	echo"หัวข้อข่าว :: ".$NewHeader."<br>";
+	echo $NewContent1;
 }
 ?>
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
