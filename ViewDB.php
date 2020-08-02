46d92bcd654fb96828f2383713464425768d88e0
Sonic4051 <keng-yok@hotmail.com>
2020-08-02 16:58:49 +0700

    Update ViewDB.php

--- ViewDB.php
+++ ViewDB.php
@@ -1,5 +1,5 @@
 <?php
-//header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 require_once('connect.php');
 if(isset($_POST['submit'])) 
 { 
@@ -17,8 +17,8 @@ if(isset($_POST['submit']))
 		}
 	}
 
-	$countStr = mb_strlen($NewContent, 'utf-8');
-	$NewContent1 = mb_substr($NewContent, 0, 1500,'utf-8');
+	$countStr = strlen($NewContent);
+	$NewContent1 = substr($NewContent, 0, 1500);
 	echo $countStr."<br>";	
 	echo"หัวข้อข่าว :: ".$NewHeader."<br>";
 	echo $NewContent1;
