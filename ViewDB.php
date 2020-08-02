41aac6a1f6a4b207149e86f7349f3f0637f552a8
Sonic4051 <keng-yok@hotmail.com>
2020-08-02 16:38:12 +0700

    Update ViewDB.php

--- ViewDB.php
+++ ViewDB.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Content-Type: text/html; charset=utf-8');
+//header('Content-Type: text/html; charset=utf-8');
 mb_internal_encoding('utf-8');
 require_once('connect.php');
 if(isset($_POST['submit'])) 
